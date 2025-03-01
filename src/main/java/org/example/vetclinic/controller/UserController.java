d32c9f4a0981cfe33cbb70edb86d69e14b1d794b
Armine Martirosyan <armine.marti@hotmail.com>
2025-03-01 11:14:56 +0000

    admin controller comment fixed/user controller fixed too

--- src/main/java/org/example/vetclinic/controller/UserController.java
+++ src/main/java/org/example/vetclinic/controller/UserController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.example.vetclinic.security.CurrentUser;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,18 +17,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @GetMapping("/menu")
-    public String menu(HttpSession session, Model model) {
+    public String menu(HttpSession session, Model model, @AuthenticationPrincipal CurrentUser currentUser) {
         String token = (String) session.getAttribute("token");
         if (token != null && !token.isEmpty()) {
             model.addAttribute("token", token);
-
         }
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated() &&
-                authentication.getPrincipal() instanceof CurrentUser currentUser) {
+        if (currentUser != null) {
             model.addAttribute("currentUser", currentUser.getUser());
-
         }
         return "/users/menu";
     }
